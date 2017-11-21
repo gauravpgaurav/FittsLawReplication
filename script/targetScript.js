0cee1ec4e283fc86f31d6da4b3e87ed0a0497b8d
Gaurav Pant <gauravpgaurav@gmail.com>
2017-11-20 20:55:41 -0600

    Structure completed

--- script/targetScript.js
+++ script/targetScript.js
@@ -5,11 +5,27 @@ $(document).ready(function() {
     var outer_Radius_Array = [100, 150, 200, 250, 300];
     var inner_Radius_Array = [20, 25, 30, 35, 40];
     var iterations = 1;
-
+    var outer_itr = 0;
+    var inner_radius = 0;
+    var outer_radius = 0;
+    inner_Radius_Array = shuffle(inner_Radius_Array);
+    outer_Radius_Array = shuffle(outer_Radius_Array);
+    
+    function updateTargetSize() {
+        inner_radius = inner_Radius_Array.pop();
+        console.log('Itr ', iterations);
+        console.log('inner_radius ', inner_radius);
+    }
+    
+    function updateTargetDistance() {
+        outer_radius = outer_Radius_Array[outer_itr];
+        console.log('Itr ', iterations);
+        console.log('outer_radius ', outer_radius);
+    }
+    
     function generateTargets() {
+        updateTargetSize();
         $('.target').remove();
-        inner_Radius_Array = shuffle(inner_Radius_Array);
-        var inner_radius = inner_Radius_Array.pop();
         targetTracker = 0;
         var container = $('#container');
         for (var i = 0; i < numberOfTargets; i++) {
@@ -21,10 +37,10 @@ $(document).ready(function() {
                 'height': inner_radius + 'px'
             }).appendTo(container);
         }
-        $("#img_0").attr('src', 'images/red.png');
-        $(".target").click(function() {
+        positionTargets();
+        $('#img_0').attr('src', 'images/red.png');
+        $('.target').click(function() {
             var currentColor = $(this).attr('src');
-            console.log("currentColor : " + currentColor);
             if (currentColor == 'images/red.png') {
                 $(this).attr('src', 'images/black_ring.png');
                 targetTracker = ($(this).attr('id')).replace(/^\D+/g, '');
@@ -32,9 +48,8 @@ $(document).ready(function() {
             }
         });
     }
-    $(".target").click(function() {
+    $('.target').click(function() {
         var currentColor = $(this).attr('src');
-        console.log("currentColor : " + currentColor);
         if (currentColor == 'images/red.png') {
             $(this).attr('src', 'images/black_ring.png');
             targetTracker = ($(this).attr('id')).replace(/^\D+/g, '');
@@ -44,12 +59,19 @@ $(document).ready(function() {
 
     function nextTarget() {
         if (targetTracker == (numberOfTargets - 1)) {
-            alert("Stage Completed ( " + iterations + " of 3) !");
-            $(".target").attr('src', 'images/black_ring.png');
+            alert('Stage Completed ( ' + iterations + ' of 6) !');
+            $('.target').attr('src', 'images/black_ring.png');
             iterations++;
-            if (iterations < 4) {
-                generateTargets();
-                positionTargets();
+            if (iterations <= 6) {
+                if ((iterations-1) % 3 == 0) {
+                    outer_itr = 0;
+                    generateTargets();
+                } else {
+                    $('.target').attr('src', 'images/black_ring.png');
+                    $('#img_0').attr('src', 'images/red.png');
+                    outer_itr++;
+                    positionTargets();
+                }
             }
             else {
                 return;
@@ -57,19 +79,20 @@ $(document).ready(function() {
         }
         var limit = numberOfTargets / 2;
         if (diametricFlag) {
-            targetTracker = targetTracker - (limit - 1);
-            $("#img_" + targetTracker).attr('src', 'images/red.png');
+            if (targetTracker != (numberOfTargets - 1)) {
+                targetTracker = targetTracker - (limit - 1);
+                $('#img_' + targetTracker).attr('src', 'images/red.png');
+            }  
             diametricFlag = false;
         } else {
             targetTracker = +targetTracker + +limit;
-            $("#img_" + targetTracker).attr('src', 'images/red.png');
+            $('#img_' + targetTracker).attr('src', 'images/red.png');
             diametricFlag = true;
         }
     }
 
     function positionTargets() {
-        outer_Radius_Array = shuffle(outer_Radius_Array);
-        var outer_radius = outer_Radius_Array.pop();
+        updateTargetDistance();
         var targets = $('.target'),
             container = $('#container'),
             width = container.width(),
